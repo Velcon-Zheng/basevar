53741dd43c37b1c402bb14d17bae9d9ff51d5eb3
Zilong-Li <zimusen94@gmail.com>
2019-04-12 10:04:59 +0800

    remove redundant code

--- include/ranksum.c
+++ include/ranksum.c
@@ -48,11 +48,11 @@ static double rankR1(double *x, int n1, double *y, int n2)
     int k = 0, n = 0;
     for (i = 0; i < n1 + n2; i++){
         if (x[i] == x[i+1]) {
-            k = k + i + 1;
+            k += i + 1;
             n++;
         }else{
             if (k > 0) {
-                k = k + i + 1;
+                k += i + 1;
                 double avg = (double)k / (n + 1);
                 for (j = i; j >= i - n; j--) {
                     x[j] = avg;
